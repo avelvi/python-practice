46b1f165af4adf808527f2e40974f9f262d9751a
Aleksey Ivlev <aleksey.velvi@gmail.com>
2018-06-27 00:10:32 +0300

    10 - 02 - Packages

---
+++ 10 - Modules and Packages/MyMainPackage/main_package_script.py
@@ -0,0 +1,2 @@
+def main_report():
+    print("I am in main_package_script in MyMainPackage package.")
---
+++ 10 - Modules and Packages/myprogram2.py
@@ -0,0 +1,6 @@
+from MyMainPackage.main_package_script import main_report
+from MyMainPackage.MySubPackage import sub_package_script
+
+main_report()
+
+sub_package_script.sub_report()
